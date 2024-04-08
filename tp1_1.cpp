cf5bfadce71919769f3e5ae672e882058f37f3c5
Thiago <thiagomagdalon@outlook.es>
2024-04-08 19:54:46 -0300

    Agregamos el archivo c

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hola mundo");
+    printf("Hola mundo \n");
+    int a = 10;
+    int *puntero = &a;
+
+    printf("%p \n", puntero);
+    printf("%p \n", &a);
+    printf("%d \n", *puntero);
+    printf("%p \n", &puntero);
+    printf("%zu \n", sizeof(a));
+
     return 0;
 }
